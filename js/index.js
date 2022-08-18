3db8a067d48c90f9e79a3a52f146f846fddd5382
Franco <francogaglianopanzardi@gmail.com>
2022-08-18 08:03:55 -0300

    Some updates
    "index.html" is "main.html" now.
    index.html redirects to main.

--- js/index.js
+++ js/index.js
@@ -1,14 +1,5 @@
-document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("autos").addEventListener("click", function() {
-        localStorage.setItem("catID", 101);
-        window.location = "products.html"
-    });
-    document.getElementById("juguetes").addEventListener("click", function() {
-        localStorage.setItem("catID", 102);
-        window.location = "products.html"
-    });
-    document.getElementById("muebles").addEventListener("click", function() {
-        localStorage.setItem("catID", 103);
-        window.location = "products.html"
-    });
-});
+document.addEventListener("click", redirect);
+
+function redirect(){
+    window.location.replace("https://frangag.github.io/ecommerce.github.io/main.html")
+}
---
+++ js/products.js
@@ -0,0 +1,5 @@
+let API = "https://japceibal.github.io/emercado-api/cats_products/101.json"
+
+getJSONData(API){
+    
+}
