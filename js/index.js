f3b9c3c3b7751349534005315d0c1ffe3d6fb15a
Franco <francogaglianopanzardi@gmail.com>
2022-08-18 11:35:57 -0300

    Fix

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
-document.addEventListener("click", redirect);
-
-function redirect(){
+document.addEventListener("click", () =>  {
     window.location.replace("https://frangag.github.io/ecommerce.github.io/main.html")
-}
+});
+
