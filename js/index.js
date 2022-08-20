f6a9cf3e7166e7909ce53da9f1b3fa9806330a18
Franco <francogaglianopanzardi@gmail.com>
2022-08-20 17:36:41 -0300

    fixes

--- js/index.js
+++ js/index.js
@@ -1,4 +1,12 @@
-document.addEventListener("click", () =>  {
-    window.location.replace("https://frangag.github.io/ecommerce.github.io/main.html")
-});
+regBtn.addEventListener("click", () =>  {
+    if(document.getElementById) {
+        window.location.replace("https://frangag.github.io/ecommerce.github.io/main.html")
 
+} else { alert("Error") //placeholder
+
+};
+
+}
+
+
+)
