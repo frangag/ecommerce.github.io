e972dc81e298a5de3d92678735c788834dce8a74
Franco <francogaglianopanzardi@gmail.com>
2022-08-20 19:26:19 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,17 @@
 regBtn.addEventListener("click", () =>  {
-    if(document.getElementById) {
-        window.location.replace("https://frangag.github.io/ecommerce.github.io/main.html")
+    let inputs = document.getElementsByTagName("input");
+    let allFields = true;
+    
+    for (let i=0; i<inputs.length;i++){
+        if (inputs[i].value.length<=0){
+            allFields = false;
+        }
+    };
 
-} else { alert("Error") //placeholder
+    if(allFields === true) {
+        window.location.replace("https://frangag.github.io/ecommerce.github.io/main.html")
+    } else {
+        alert("Debe ingresar valores en ambos campos")
 
 };
 
