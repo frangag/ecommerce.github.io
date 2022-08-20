96a3366768bf9c2b836698a2ad7214b604a3defe
Franco <francogaglianopanzardi@gmail.com>
2022-08-20 01:43:34 -0300

    updates

--- js/products.js
+++ js/products.js
@@ -1,5 +1,16 @@
 let API = "https://japceibal.github.io/emercado-api/cats_products/101.json"
 
-getJSONData(API){
-    
-}
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(CATEGORIES_URL).then(function(resultObj){
+        if (resultObj.status === "ok"){
+            
+        }
+});
+})
+/* async function loadData(){
+    let response = await fetch(API);
+    let json = await response.json();
+    console.log(json.products);
+    }
+
+loadData(); */
